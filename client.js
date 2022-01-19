b0d50b90a9f610ee8ddac2e76ba81b3edff00dba
Shauna Empringham <shaunathedead86@gmail.com>
2022-01-19 09:05:57 -0500

    adding connect and data handling functionality to client.js

--- client.js
+++ client.js
@@ -10,6 +10,11 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on("connect", () => {
+    console.log('Connection successful!');
+    conn.write('Name: SHA');
+  });
+
   conn.on("data", (data) => {
     console.log(data);
   });
